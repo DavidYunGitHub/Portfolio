7c040adcc05b19188e8671ead645cfb109056e5f
David Yun <yunpro@Davids-MacBook-Pro.local>
2020-04-14 19:13:05 -0500

    Updates

--- precache-manifest.74a19e725511efa54e11ce9ed937307f.js
+++ precache-manifest.74a19e725511efa54e11ce9ed937307f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8e1a3a782e8619c3dc730d6c7cd8de22",
+    "revision": "8418086702e5a98638f7a7d6e20ec43c",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.559fd73f.chunk.css"
   },
   {
-    "revision": "0a597c6cf7ad40837c20",
+    "revision": "76b5e95de7484e3be22b",
     "url": "/portfolio/static/css/main.29da4bdb.chunk.css"
   },
   {
@@ -16,16 +16,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.3c8c844d.chunk.js"
   },
   {
-    "revision": "0a597c6cf7ad40837c20",
-    "url": "/portfolio/static/js/main.7c92e64a.chunk.js"
+    "revision": "76b5e95de7484e3be22b",
+    "url": "/portfolio/static/js/main.418134cc.chunk.js"
   },
   {
     "revision": "a1e33bd9235cc61d13b1",
     "url": "/portfolio/static/js/runtime-main.8ecab1cc.js"
   },
   {
-    "revision": "d9b51b304e93b4df3a2a9c67daeee011",
-    "url": "/portfolio/static/media/David Yun Resume.d9b51b30.pdf"
+    "revision": "543f9becdca6775c1a6794c910b26ad2",
+    "url": "/portfolio/static/media/David Yun Resume.543f9bec.pdf"
   },
   {
     "revision": "2fa5163b754f4c3cdd84b31896fff19a",
@@ -64,8 +64,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/media/recipe.5a418699.png"
   },
   {
-    "revision": "e8646482a18cd40784bbec8ed7962a5d",
-    "url": "/portfolio/static/media/resume_ss.e8646482.png"
+    "revision": "8db8b789cf6c8e82f321e6c0d374792a",
+    "url": "/portfolio/static/media/resume.8db8b789.png"
   },
   {
     "revision": "3b941f2db7e5611fd518710b5d4312b7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d1015acf36d41b3ed18c3fba848c410d.js"
+  "/portfolio/precache-manifest.74a19e725511efa54e11ce9ed937307f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
